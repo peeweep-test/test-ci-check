8544cbbccd29924c1e0b186a7cb2c770fe5a7ae0
kuchune <kuchune@uniontech.com>
2024-08-14 17:46:18 +0800

    fix: add test file

--- test.go
+++ test.go
@@ -1,9 +1,26 @@
 package main
-import "fmt"
-func main() {
-    var a string = "Runoob"
-    fmt.Println(a)
 
-    var d int = 1, 2
-    fmt.rintln(b, c)
+import (
+    "errors"
+    "fmt"
+    "math"
+)
+
+func circleArea(radius float64) (float64, error) {
+    if radius < 0 { 		// 如果小于0则运行以下
+        return 0, errors.New("Area calculation failed, radius is less than zero")
+    }
+    return math.Pi * radius * radius, nil
 }
+
+func main() {
+    radius := -10.00
+    area, err := circleArea(radius)
+    if err != nil { 		// 如果为空
+        fmt.Println(err)
+        return
+    }
+    fmt.Printf("Area of circle %0.2f", area)
+}
+
+// Area calculation failed, radius is less than zero
